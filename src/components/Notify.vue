680cb88e1537b1feb58c94ed79f61162655fe885
Dmitrii Chizhov <dimachig@gmail.com>
2022-12-15 15:14:59 +0300

    add: scss styles in Notify component

--- src/components/Notify.vue
+++ src/components/Notify.vue
@@ -24,5 +24,27 @@ export default {
     }
   }
 }
-
 </script>
+<style lang="scss" scoped>
+table {
+  text-align: center;
+  td{
+    display: flex;
+    justify-content: space-between;
+    padding: 12px 6px;
+    font-size: 14px;
+  }
+}
+/* Animation */
+.list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+</style>
