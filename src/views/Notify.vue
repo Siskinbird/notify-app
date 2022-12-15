838588526e01ffe2dc446262747bad24f97291e1
Dmitrii Chizhov <dimachig@gmail.com>
2022-12-15 15:09:28 +0300

    add: Notify component width props

--- src/views/Notify.vue
+++ src/views/Notify.vue
@@ -1,9 +1,28 @@
 <template>
-  <div class="wrapper-content wrapper-content--fixed">
-    <section>
-      <div class="container">
-        <h1>Example Page</h1>
-      </div>
-    </section>
-  </div>
+  <table>
+    <transition-group name="list" tag="tbody">
+      <tr v-for="message in messages" :key="message.title">
+        <td><span>{{message.title}}</span></td>
+      </tr>
+    </transition-group>
+  </table>
+<!--  <div class="wrapper-content wrapper-content&#45;&#45;fixed">-->
+<!--    <section>-->
+<!--      <div class="container">-->
+<!--        <h1>Example Page</h1>-->
+<!--      </div>-->
+<!--    </section>-->
+<!--  </div>-->
 </template>
+
+<script>
+export default {
+  props: {
+    messages: {
+      type: Array,
+      required: true
+    }
+  }
+}
+
+</script>
